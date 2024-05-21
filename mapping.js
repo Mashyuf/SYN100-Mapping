f1ac0e41337eeba9c7e8fd15f83f45a4425a17ee
Matthew Fang <64571966+Mashyuf@users.noreply.github.com>
2024-05-21 16:13:50 -0700

    Update Code and Files

--- mapping.js
+++ mapping.js
@@ -1,8 +1,31 @@
 const fs = require('node:fs');
 
+const options = {
+    ignoreAttributes: false
+};
+
+const { XMLParser, XMLBuilder, XMLValidator } = require('fast-xml-parser');
+
 try {
-    const data = fs.readFileSync('Personal Income by County - 2002.kml');
+    const data = fs.readFileSync('map.kml', "utf8");
+    const parser = new XMLParser(options);
+    let jObj = parser.parse(data);
     console.log('read file success');
+    const rootFolder = jObj.kml.Document.Folder;
+    console.log(rootFolder.Folder[0].Placemark.MultiGeometry.Polygon);
+
+
+
+    const builder = new XMLBuilder(options);
+    let xml = builder.build(jObj);
+
+    try {
+        fs.writeFileSync('test.kml', xml)
+        console.log('write file success');
+    } catch (err) {
+        console.log(err);
+    }
 } catch (err) {
     console.log(err);
 }
+

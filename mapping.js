6e689526de03178a6eefb5c8cbb7b42bce238a47
Matthew Fang <64571966+Mashyuf@users.noreply.github.com>
2024-05-21 17:11:08 -0700

    Added Wisconsin County Names

--- mapping.js
+++ mapping.js
@@ -1,20 +1,21 @@
 const fs = require('node:fs');
-
 const options = {
     ignoreAttributes: false
 };
-
 const { XMLParser, XMLBuilder, XMLValidator } = require('fast-xml-parser');
 
 try {
-    const data = fs.readFileSync('map.kml', "utf8");
+    const data = fs.readFileSync('map.kml', 'utf8');
+    const wisconsin = fs.readFileSync('Table.csv', 'utf8');
     const parser = new XMLParser(options);
     let jObj = parser.parse(data);
     console.log('read file success');
     const rootFolder = jObj.kml.Document.Folder;
-    console.log(rootFolder.Folder[0].Placemark.MultiGeometry.Polygon);
-
-
+    let wisconsinArr = csvToArr(wisconsin, ',');
+    for(let i = 0; i < wisconsinArr.length; i++) {
+        rootFolder.Folder[i] = Object.assign(rootFolder.Folder[i], {name: wisconsinArr[i].GeoName})
+    }
+    console.log(typeof(rootFolder.Folder[0]));
 
     const builder = new XMLBuilder(options);
     let xml = builder.build(jObj);
@@ -29,3 +30,16 @@ try {
     console.log(err);
 }
 
+function csvToArr(stringVal, splitter) {
+    const [keys, ...rest] = stringVal
+      .trim()
+      .split("\r\n")
+      .map((item) => item.split(splitter));
+  
+    const formedArr = rest.map((item) => {
+      const object = {};
+      keys.forEach((key, index) => (object[key] = item.at(index)));
+      return object;
+    });
+    return formedArr;
+  }
